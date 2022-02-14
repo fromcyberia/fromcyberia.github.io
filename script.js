e33530daf88abd7cb5981c639701cb0fbb797e8a
Silvestre Iwakura <88948872+fromcyberia@users.noreply.github.com>
2022-02-14 02:39:49 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+      var hydra = new Hydra({
+        canvas: document.getElementById("myCanvas"),
+        detectAudio: false,
+        enableStreamCapture: false,
+      })
+osc(20, 0.01, 1.1)
+	.kaleid(5)
+	.color(2.83,0.91,0.39)
+	.rotate(0, 0.1)
+	.modulate(o0, () => mouse.x * 0.0003)
+	.scale(1.01)
+  	.out(o0)
