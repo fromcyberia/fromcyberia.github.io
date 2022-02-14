ea7c51fd2bf4724d529d16833fca45fca97b0238
Silvestre Iwakura <88948872+fromcyberia@users.noreply.github.com>
2022-02-14 11:58:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ osc(20, 0.01, 1.1)
 	.scale(1.01)
   	.out(o0)
 
+p1 = new P5()
+//write your own poem here
+textArray = ["hay golpes en la vida", "tan fuertes", "yo no sé", "golpes como los del odio de dios"]
+
+
+p1.draw = () => {
+ if (p1.mouseIsPressed){
+ p1.fill(p1.random(255),p1.random(255),p1.random(255));
+ } else {
+  p1.fill(p1.random(255),p1.random(255),p1.random(255));
+  }
+  p1.clear()
+  index = Math.floor(time%textArray.length)
+  p1.textSize(80)
+  p1.stroke(p1.random(255),p1.random(255),p1.random(255));
+  p1.strokeWeight(7.5)
+  p1.textFont('Courier');
+  p1.text(textArray[index],p1.mouseX,p1.mouseY)
+}
+
